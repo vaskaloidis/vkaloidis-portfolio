981a8b5490c1fc0f768e0e7b830a00a98a7fe601
Vas Kaloidis <vas.kaloidis@gmail.com>
2016-10-15 20:47:44 -0400

    Switched auth credential parser to different api for loading environment variables

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
-	@username = ENV["username"]
-	@password = ENV["password"]
+	@username = Rails.env.username
+	@password = Rails.env.password
 
   http_basic_authenticate_with name: @username, password: @password
   before_action :set_project, only: [:show, :edit, :update, :destroy, :create]
