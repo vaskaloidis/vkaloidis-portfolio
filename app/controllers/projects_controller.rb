9f656097c23629eefc01fd8d924a37d4130ba88e
Vas Kaloidis <vas.kaloidis@gmail.com>
2017-10-04 14:39:29 -0400

    Updated auth info

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -78,7 +78,7 @@ class ProjectsController < AdminController
   private
   def authenticate
     authenticate_or_request_with_http_basic do |user_name, password|
-      session[:admin] = (user_name == 'Maxima2017!' && password == 'Maxima2017!')
+      session[:admin] = (user_name == 'vas' && password == 'Maxima2017!')
     end
   end
 
