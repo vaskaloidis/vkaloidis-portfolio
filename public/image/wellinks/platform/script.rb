7a721f522c6e2d8d5c3d83361566e1713aa232c4
Vas Kaloidis <vas.kaloidis@gmail.com>
2023-06-08 13:09:25 -0400

    Added platform screenshots

---
+++ public/image/wellinks/platform/script.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+(1..12).each do |digit|
+  puts "<a href=\"/image/wellinks/platform/platform#{digit}.png\" data-caption=\"Backend API Server\">
+        <img class=\"img-thumb\" src=\"/image/wellinks/platform/platform#{digit}.png\" alt=\"Wellinks\">
+      </a>
+      "
+end
